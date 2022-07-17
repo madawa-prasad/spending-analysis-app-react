04b76367d3bcdc34d8842d2cbd68f1f167cd0a60
madawa-prasad <63942791+madawa-prasad@users.noreply.github.com>
2022-07-17 12:55:33 +0530

    Add navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-light bg-light shadow p-3 mb-5">
-      <div class="container-fluid">
+    <nav className="navbar navbar-light bg-light shadow p-3 mb-5">
+      <div className="container-fluid">
         <span className="navbar-brand">
           <img
             src="https://www.seekpng.com/png/full/75-755344_png-file-high-cost-icon-png.png"
             alt=""
             width="30"
             height="30"
-            class="d-inline-block align-text-top"
+            className="d-inline-block align-text-top"
           />
           <span className="ms-3 font-weight-bold">Spending Analysis App</span>
         </span>
