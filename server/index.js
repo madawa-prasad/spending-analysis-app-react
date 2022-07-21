543f551ea8d83c31fe18f4fc3e472c81df800476
madawa-prasad <63942791+madawa-prasad@users.noreply.github.com>
2022-07-21 21:18:42 +0530

    Add fix in get all transactions queries

--- server/index.js
+++ server/index.js
@@ -136,8 +136,8 @@ app.get('/transactions/:id', async (req, res) => {
   try {
     const id = parseInt(req.params.id);
     const allTransactions = await pool.query(
-      'SELECT COALESCE(sum_i.tr_date,sum_e.tr_date) AS tr_date, COALESCE(sum_i.incomes_sum,0) AS incomes, COALESCE(sum_e.expenses_sum,0) AS expenses FROM (SELECT tr_date, SUM(tr_amount) AS incomes_sum FROM transactions WHERE tr_is_income=TRUE AND est_id=$1 GROUP BY tr_date) sum_i FULL OUTER JOIN (SELECT tr_date, SUM(tr_amount) AS expenses_sum FROM transactions WHERE tr_is_income=FALSE AND est_id=$2 GROUP BY tr_date) sum_e ON (sum_i.tr_date = sum_e.tr_date)',
-      [id, id]
+      'SELECT * FROM transactions INNER JOIN categories ON transactions.tr_category = categories.cat_id WHERE est_id = $1 ORDER BY tr_date ASC',
+      [id]
     );
     res.json(allTransactions.rows);
   } catch (err) {
@@ -201,8 +201,8 @@ app.get('/sumsD/:est_id', async (req, res) => {
   try {
     const est_id = req.params.est_id;
     const categories = await pool.query(
-      'SELECT tr_date, CASE WHEN tr_is_income = true THEN SUM(tr_amount) ELSE 0 END AS income_sum,CASE WHEN tr_is_income = false THEN SUM(tr_amount) ELSE 0 END AS expense_sum FROM transactions WHERE est_id=$1 GROUP BY tr_date , tr_is_income',
-      [est_id]
+      'SELECT COALESCE(sum_i.tr_date,sum_e.tr_date) AS tr_date, COALESCE(sum_i.incomes_sum,0) AS incomes, COALESCE(sum_e.expenses_sum,0) AS expenses FROM (SELECT tr_date, SUM(tr_amount) AS incomes_sum FROM transactions WHERE tr_is_income=TRUE AND est_id=$1 GROUP BY tr_date) sum_i FULL OUTER JOIN (SELECT tr_date, SUM(tr_amount) AS expenses_sum FROM transactions WHERE tr_is_income=FALSE AND est_id=$2 GROUP BY tr_date) sum_e ON (sum_i.tr_date = sum_e.tr_date)',
+      [est_id, est_id]
     );
     res.json(categories.rows);
   } catch (err) {
